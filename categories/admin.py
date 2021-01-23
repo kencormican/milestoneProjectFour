808e8463e07c76c57dc5d8c4a9d14685400e9140
kencormican <ken.cormican@bt.com>
2021-01-23 15:50:36 +0000

    Category fixtures, models and admin updated.

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Category, Subcategory
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'pk',
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('pk',)
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'pk',
+        'friendly_name',
+        'name',
+        'new'
+    )
+
+    ordering = ('pk',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
