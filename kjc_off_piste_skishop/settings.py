4a7e1ef09d2d4fcd988a9164e697a45de30e0950
kencormican <ken.cormican@bt.com>
2021-01-25 16:11:11 +0000

    Error correcting emailbackend syntax

--- kjc_off_piste_skishop/settings.py
+++ kjc_off_piste_skishop/settings.py
@@ -206,7 +206,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-if 'development':
+if development:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -216,14 +216,11 @@ if 'development':
     # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-elif 'production':
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
