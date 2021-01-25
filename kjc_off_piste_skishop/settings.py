34568b63d5b60d9d907bcc97b5484a5d325d89c0
kencormican <ken.cormican@bt.com>
2021-01-25 15:50:26 +0000

    Explicit reference to production enviornment for calling emailbackend

--- kjc_off_piste_skishop/settings.py
+++ kjc_off_piste_skishop/settings.py
@@ -216,11 +216,14 @@ if 'development':
     # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-else:
+elif 'production':
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
