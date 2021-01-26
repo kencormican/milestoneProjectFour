e89cf2218ca9498c3ca127c60e5b464aa5bf7150
kencormican <ken.cormican@bt.com>
2021-01-26 14:05:18 +0000

    Heroku Debug Test....500 Server issue.#5- modified STATICFILES_DIRS to include media

--- kjc_off_piste_skishop/settings.py
+++ kjc_off_piste_skishop/settings.py
@@ -197,7 +197,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(
-    BASE_DIR, 'static'),)
+    BASE_DIR, 'static', 'media'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
