8acced57a74e5354d44d946fd8079cf8b8911a38
kencormican <ken.cormican@bt.com>
2021-01-26 22:13:00 +0000

    Product detail views and urls wired up to basic template. Styles customised to off-piste theme.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 # from categories.models import Category, Subcategory
@@ -8,11 +8,13 @@ from .models import Product
 # Create your views here.
 
 def products(request):
-    """ This view renders the products pages and allow will
-    ultimately allow editing and deletion of the products in
-    by the admin"""
+    """ This view renders the products pages and will ultimately
+    allow editing and deletion of the products in by the admin"""
 
     products = Product.objects.all()
+
+    for product in products:
+        product.name = product.name.lower()
 
     context = {
         'products': products,
@@ -20,3 +22,18 @@ def products(request):
 
     return render(request, 'products/products.html', context)
 
+
+def product_detail(request, product_id):
+    """ This view renders a product's detailed information & will
+    ultimately allow editing and deletion of the product by the admin"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.name = product.name.lower()
+    product.summary = product.summary.lower()
+    product.detail = product.summary.lower()
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
