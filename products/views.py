29140c5ba22cd0250fe1107b3a3e338f796c34c6
kencormican <ken.cormican@bt.com>
2021-01-28 18:19:46 +0000

    Edit Product templates and views wired up.

--- products/views.py
+++ products/views.py
@@ -121,3 +121,33 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ This view renders the edit product template and allows
+    the update of the product on POST"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f"You've Successfully updated {product.name}")
+            return redirect(reverse('product_detail', args=[product_id]))
+        else:
+            messages.error(
+                request, "Update Product Failed. Please check form is valid."
+            )
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
