ff27d899115629935227521bf0e9f7bcacdee385
kencormican <ken.cormican@bt.com>
2021-01-30 13:23:13 +0000

    setting change to test  migrations on heroku

--- kjc_off_piste_skishop/settings.py
+++ kjc_off_piste_skishop/settings.py
@@ -158,18 +158,18 @@ WSGI_APPLICATION = 'kjc_off_piste_skishop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if production:
-    DATABASES = {'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'))}
+# if production:
+DATABASES = {'default': dj_database_url.parse(
+    os.environ.get('DATABASE_URL'))}
 
-else:
-    print("Development Environment. Using SQLite")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# else:
+#     print("Development Environment. Using SQLite")
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation
