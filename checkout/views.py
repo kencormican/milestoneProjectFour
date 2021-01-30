5ee2ba8a271a383d213e61c57aea9f82272b1867
kencormican <ken.cormican@bt.com>
2021-01-30 14:25:10 +0000

    bag items wired up to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
 from django.contrib import messages
+
+from .forms import OrderForm
 
 
 # Create your views here.
 
 def checkout(request):
-    """ This view renders the checkout """
+    """ This view renders the checkout page """
+    bag = request.session.get('bag', {})
 
-    return render(request, 'checkout/checkout.html')
+    # defensive logic
+    if not bag:
+        messages.error(request, "Your bag is currently empty")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
