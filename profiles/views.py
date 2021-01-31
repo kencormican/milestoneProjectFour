486aa536b77c81d1eec4511172da4511d5fb1805
kencormican <ken.cormican@bt.com>
2021-01-31 16:43:57 +0000

    Order History wired up to profile and checkout views. Testing Clean

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, reverse, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 # Create your views here.
@@ -18,15 +19,38 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. \
+            Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
-    form = UserProfileForm(instance=profile)
-
+    orders = profile.orders.all()
     template = 'profiles/profile.html'
+
     context = {
         'form': form,
-        'profile': profile,
+        'orders': orders,
+        'on_profile_page': True
     }
     messages.info(request, ("""Update your profile details using the
             form below"""))
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
 
     return render(request, template, context)
